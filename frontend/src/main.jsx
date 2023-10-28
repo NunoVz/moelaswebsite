3ae7f0c23c0fcad065b2aeb65fd1869974b134a2
Hugo Barros <hugosbarros@gmail.com>
2023-10-28 21:25:51 +0100

    Changed React Router Dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 import App from './App.jsx'
 import Shots from './pages/Shots/Shots.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/shots',
+        element: <Shots />,
+      },
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/shots" element={<Shots />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
