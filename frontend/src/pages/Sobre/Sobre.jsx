a6d5f06c738a641cda3ebda3631e55f5ddaf43f8
Hugo Barros <hugosbarros@gmail.com>
2023-12-08 23:42:04 +0000

    ya done

--- frontend/src/pages/Sobre/Sobre.jsx
+++ frontend/src/pages/Sobre/Sobre.jsx
@@ -4,15 +4,18 @@ export default function Sobre() {
 	return (
 		<div className="sobre" id="sobre">
 			<span>
-				Descubre a história única e envolvente de O Moelas, um ícone na vida noturna de Coimbra. Fundado em 1974 como Auto Serviço da Sé, o bar recebeu o reconhecimento oficial da Câmara de Coimbra como entidade de interesse histórico e cultural. Em 1998, adotou o nome atual devido à fama das suas moelas, marcando o início de uma tradição que perdura até hoje.
+				O Moelas, um símbolo de história, tradição e diversão! Fundado em 1974 como Auto Serviço da Sé, este bar, é desde então um ícone na vida noturna de Coimbra. Em 1998, adotou o nome atual devido à fama das suas moelas, marcando assim o início de uma tradição que perdura até aos dias de hoje.
+
 			</span>
 			<br />
 			<span>
-				Classificado pelo Conselho de Veteranos como Tasca de Interesse Histórico em 2019, O Moelas tornou-se o único local permitido para praxes debaixo de telha, à exceção de repúblicas. Conheça a história por trás deste reconhecimento e saiba como este estatuto beneficia o estabelecimento, garantindo a preservação das suas tradições.
+				Classificado, pelo Conselho de Veteranos, como Tasca de Interesse Histórico, O Moelas tornou-se, em 2019, um dos poucos locais onde que é permitida a praxe debaixo de telha. É também reconhecido como entidade de interesse histórico e cultural, valorização atribuída em 2021 pela Câmara Municipal de Coimbra.
+
 			</span>
 			<br />
 			<span>
-				Explora a variedade de bebidas exclusivas criadas por O Moelas. Desde shots de curso até às misteriosas misturas como Xicos e Beneno, cada bebida conta uma história única.
+				Explora a variedade de bebidas exclusivas criadas pel'O Moelas. Desde shots de curso até às misteriosas misturas como Xicos e Beneno, cada bebida conta uma história única!
+
 			</span>
 		</div>
 	)
